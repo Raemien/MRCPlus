f5e6b0d2e091ffaabeaceb1413334d40a804f33a
Raemien <39755457+Raemien@users.noreply.github.com>
2021-04-18 00:57:28 +1200

    Version bump dependencies

--- Android.mk
+++ Android.mk
@@ -24,26 +24,26 @@ LOCAL_MODULE := modloader
 LOCAL_EXPORT_C_INCLUDES := extern/modloader
 LOCAL_SRC_FILES := extern/libmodloader.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: beatsaber-hook - version: 1.2.4
+# Creating prebuilt for dependency: beatsaber-hook - version: 1.3.3
 include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_1_2_4
+LOCAL_MODULE := beatsaber-hook_1_3_3
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_2_4.so
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_3_3.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: codegen
+# Creating prebuilt for dependency: codegen - version: 0.7.2
 include $(CLEAR_VARS)
-LOCAL_MODULE := codegen_0_7_0
+LOCAL_MODULE := codegen_0_7_2
 LOCAL_EXPORT_C_INCLUDES := extern/codegen
-LOCAL_SRC_FILES := extern/libcodegen_0_7_0.so
+LOCAL_SRC_FILES := extern/libcodegen_0_7_2.so
 LOCAL_CPP_FEATURES += rtti exceptions
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: questui - version: 0.5.0
+# Creating prebuilt for dependency: questui - version: 0.6.7
 include $(CLEAR_VARS)
 LOCAL_MODULE := questui
 LOCAL_EXPORT_C_INCLUDES := extern/questui
 LOCAL_SRC_FILES := extern/libquestui.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: custom-types - version: 0.5.1
+# Creating prebuilt for dependency: custom-types - version: 0.8.3
 include $(CLEAR_VARS)
 LOCAL_MODULE := custom-types
 LOCAL_EXPORT_C_INCLUDES := extern/custom-types
@@ -59,8 +59,8 @@ LOCAL_SRC_FILES += $(call rwildcard,src/,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_2_4
-LOCAL_SHARED_LIBRARIES += codegen_0_7_0
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_3_3
+LOCAL_SHARED_LIBRARIES += codegen_0_7_2
 LOCAL_SHARED_LIBRARIES += questui
 LOCAL_SHARED_LIBRARIES += custom-types
 LOCAL_LDLIBS += -llog
--- buildQmod.ps1
+++ buildQmod.ps1
@@ -7,8 +7,9 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
 }
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk -j 4
-Compress-Archive -Path  "./libs/arm64-v8a/libbeatsaber-hook_1_2_4.so",`
+Compress-Archive -Path  "./libs/arm64-v8a/libbeatsaber-hook_1_3_3.so",`
                         "./libs/arm64-v8a/libmrcplus.so",`
+                        "./cover.png",`
                         "./mod.json" -DestinationPath "./MRCPlus.zip" -Update
 
 & copy-item -Force "./MRCPlus.zip" "./MRCPlus.qmod"
