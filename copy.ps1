02c09d9d7de538c41d2b34490b4e57c689ab3c6f
raineio <64136899+raineio@users.noreply.github.com>
2022-01-30 02:07:29 +0100

    Migration to qpm-rust & CMake + Ninja-Build

--- copy.ps1
+++ copy.ps1
@@ -1,10 +1,59 @@
-$NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
+Param(
+    [Parameter(Mandatory=$false)]
+    [Switch] $clean,
 
-$buildScript = "$NDKPath/build/ndk-build"
-if (-not ($PSVersionTable.PSEdition -eq "Core")) {
-    $buildScript += ".cmd"
+    [Parameter(Mandatory=$false)]
+    [Switch] $log,
+
+    [Parameter(Mandatory=$false)]
+    [Switch] $useDebug,
+
+    [Parameter(Mandatory=$false)]
+    [Switch] $self,
+
+    [Parameter(Mandatory=$false)]
+    [Switch] $all,
+
+    [Parameter(Mandatory=$false)]
+    [String] $custom="",
+
+    [Parameter(Mandatory=$false)]
+    [Switch] $file,
+
+    [Parameter(Mandatory=$false)]
+    [Switch] $help
+)
+
+if ($help -eq $true) {
+    echo "`"Copy`" - Builds and copies your mod to your quest, and also starts Beat Saber with optional logging"
+    echo "`n-- Arguments --`n"
+
+    echo "-Clean `t`t Performs a clean build (equvilant to running `"Build -clean`")"
+    echo "-UseDebug `t Copied the debug version of the mod to your quest"
+    echo "-Log `t`t Logs Beat Saber using the `"Start-Logging`" command"
+
+    echo "`n-- Logging Arguments --`n"
+
+    & $PSScriptRoot/start-logging.ps1 -help -excludeHeader
+
+    exit
 }
 
-& $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
-& adb push libs/arm64-v8a/libmrcplus.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libmrcplus.so
-& adb shell am force-stop com.beatgames.beatsaber
+& $PSScriptRoot/build.ps1 -clean:$clean
+
+if ($LASTEXITCODE -ne 0) {
+    echo "Failed to build, exiting..."
+    exit $LASTEXITCODE
+}
+
+if ($useDebug -eq $true) {
+    $fileName = Get-ChildItem lib*.so -Path "build/debug" -Name
+} else {
+    $fileName = Get-ChildItem lib*.so -Path "build/" -Name
+}
+
+& adb push build/$fileName /sdcard/Android/data/com.beatgames.beatsaber/files/mods/$fileName
+
+& $PSScriptRoot/restart-game.ps1
+
+if ($log -eq $true) { & $PSScriptRoot/start-logging.ps1 -self:$self -all:$all -custom:$custom -file:$file }
